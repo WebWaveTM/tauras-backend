3932c1505068afa1975c8b55fe6b09a89e658b3e
Kirill Naumenko <naumenkokirill466@gmail.com>
2025-05-27 02:51:57 +0300

    refactor: update config schema

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -1,17 +1,30 @@
 import { Logger } from '@nestjs/common';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { existsSync, readFileSync, statSync } from 'fs';
+import { isAbsolute, resolve } from 'path';
 import z from 'zod';
 
-function read(filePath: string) {
+function resolveKey(value: string, keyName: string): string {
   const logger = new Logger('InitConfig');
-  const path = resolve(__dirname, filePath);
-  try {
-    return readFileSync(path, { encoding: 'utf-8' });
-  } catch (error) {
-    logger.error(`Error reading file at ${path}:`, error);
-    throw new Error(`Failed to read file at ${path}`);
+  if (!value) {
+    logger.error(`${keyName} is not provided`);
+    throw new Error(`${keyName} is not provided`);
   }
+
+  const maybePath = isAbsolute(value) ? value : resolve(__dirname, value);
+  if (existsSync(maybePath) && statSync(maybePath).isFile()) {
+    try {
+      return readFileSync(maybePath, { encoding: 'utf-8' });
+    } catch (error) {
+      logger.error(`Error reading ${keyName} at ${maybePath}:`, error);
+      throw new Error(`Failed to read ${keyName} at ${maybePath}`);
+    }
+  }
+
+  if (value.trim().length === 0) {
+    logger.error(`${keyName} is empty`);
+    throw new Error(`${keyName} is empty`);
+  }
+  return value;
 }
 
 const portSchema = z
@@ -24,15 +37,19 @@ export const ConfigSchema = z.object({
     .string()
     .transform((val) => +val)
     .pipe(z.number().int().min(0)),
-  ACCESS_PRIVATE_KEY: z.string().transform(read),
-  ACCESS_PUBLIC_KEY: z.string().transform(read),
+  ACCESS_PRIVATE_KEY: z
+    .string()
+    .transform((val) => resolveKey(val, 'ACCESS_PRIVATE_KEY')),
+  ACCESS_PUBLIC_KEY: z
+    .string()
+    .transform((val) => resolveKey(val, 'ACCESS_PUBLIC_KEY')),
   ADMIN_EMAIL: z.string().email().optional(),
   ADMIN_PASSWORD: z.string().min(8).optional(),
   APP_PORT: portSchema,
   BASE_URL: z.string().url(),
   CORS_ORIGIN: z
     .string()
-    .transform((val) => val.split(',').map((item) => item.trim()))
+    .transform((val) => val.split(';').map((item) => item.trim()))
     .pipe(z.array(z.string().url()))
     .optional()
     .default('http://localhost:5173'),
@@ -70,8 +87,12 @@ export const ConfigSchema = z.object({
     .string()
     .transform((val) => +val)
     .pipe(z.number().int().min(0)),
-  REFRESH_PRIVATE_KEY: z.string().transform(read),
-  REFRESH_PUBLIC_KEY: z.string().transform(read),
+  REFRESH_PRIVATE_KEY: z
+    .string()
+    .transform((val) => resolveKey(val, 'REFRESH_PRIVATE_KEY')),
+  REFRESH_PUBLIC_KEY: z
+    .string()
+    .transform((val) => resolveKey(val, 'REFRESH_PUBLIC_KEY')),
   RESTORE_PASSWORD_LINK: z.string().url(),
   RESTORE_PASSWORD_TOKEN_LIFETIME: z
     .string()
